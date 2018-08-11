25529316b8af07cc530a78068803653bc2461e68
Adriano <adriano.interaminense@gmail.com>
2018-08-11 14:35:25 -0300

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,9 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
-const extractSass = new ExtractTextWebpackPlugin({
-  filename: '[name].[contenthash:8].bundle.css',
-  disable: false,
-});
-
-const config = {
-  entry: './src/index.js',
+module.exports = {
+  entry: './src/pixeleasier.js',
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.html'),
-    }),
-    extractSass,
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015'],
-        },
-      },
-      {
-        test: /\.s[ca]ss$/,
-        loader: extractSass.extract({
-          loader: [
-            { loader: 'css-loader' },
-            { loader: 'sass-loader' },
-          ],
-          fallbackLoader: 'style-loader',
-        }),
-      },
-    ],
-  },
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'pixeleasier.js'
+  }
 };
-
-module.exports = config;
