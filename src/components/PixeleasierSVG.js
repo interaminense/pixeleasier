02eebc6d26e9236e82f38eed5d4db615f0931a7b
Adriano <adriano.interaminense@gmail.com>
2018-08-12 14:30:44 -0300

    versin 1.0.0

---
+++ src/components/PixeleasierSVG.js
@@ -0,0 +1,49 @@
+import Pixeleasier from '../pixeleasier';
+import { createElement, setAttr } from '../utils/dom';
+import { classes } from '../utils/constants';
+
+class PixeleasierSVG extends Pixeleasier {
+	createDraw() {
+		const {draw} = classes;
+		const elementDraw = createElement('svg', draw);
+
+		setAttr(elementDraw, {
+			width: this.width,
+			height: this.height
+		});
+
+		return elementDraw;
+	}
+
+	createPixel(pixelColor, i, j) {
+		const {pixel} = classes;
+		const pixelElement = createElement('rect', pixel);
+
+		setAttr(pixelElement, {
+			style: `fill:${pixelColor}`,
+			width: this.size,
+			height: this.size,
+			x: this.size * j,
+			y: this.size * i
+		});
+
+		return pixelElement;
+	}
+
+	createDrawing() {
+		const draw = this.createDraw();
+
+		this.arr.forEach((rows, i) => {
+			rows.forEach((pixelColor, j) => {
+				const pixel = this.createPixel(pixelColor, i, j);
+
+				draw.appendChild(pixel);
+			});
+
+		});
+
+		return draw;
+	}
+}
+
+export default PixeleasierSVG;
