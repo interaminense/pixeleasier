652f7e74185c85f1131ae92a3fd3adbe80925d8d
Adriano <adriano.interaminense@gmail.com>
2018-08-16 22:38:38 -0300

    SF

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'pixeleasier.js'
-  }
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'pixeleasier.js'
+	}
 };
