7a21d93539da13e0946a04b3e804b57142d54c61
Adriano <adriano.interaminense@gmail.com>
2018-08-16 23:25:46 -0300

    test travis

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Pixeleasier from './components/Pixeleasier';
 import PixeleasierCSS from './components/PixeleasierCSS';
 import PixeleasierSVG from './components/PixeleasierSVG';
-
+ 
 export {Pixeleasier, PixeleasierCSS, PixeleasierSVG};
 export default Pixeleasier;
